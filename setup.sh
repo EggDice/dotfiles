bed742ac0ee9f384f01300ea2334cab34019c7a2
Egg Dice <egg@BunnyBalloon>
2014-06-06 16:55:11 +0200

    change to i3, linux setup script

---
+++ setup.sh
@@ -0,0 +1,25 @@
+sudo apt-get install aptitude
+sudo aptitude update
+sudo aptitude upgrade
+sudo aptitude install -y vim git tmux i3
+sudo aptitude install -y lightdm
+
+# Google Chrome
+wget -c wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-*.deb
+sudo apt-get -y install -f
+rm google-chrome-*.deb
+
+# WiFi
+sudo aptitude install -y linux-headers-$(uname -r|sed 's,[^-]*-[^-]*-,,') broadcom-sta-dkms
+sudo modprobe -r b44 b43 b43legacy ssb brcmsmac
+sudo modprobe wl
+sudo usermod -G netdev -a egg
+
+# Desktop
+mkdir video
+mkdir pictures
+mkdir downloads 
+mkdir music
+mkdir projects
+mkdir brickflow
---
+++ sudo.sh
@@ -0,0 +1,1 @@
+su root -c "cp sources.list /etc/apt/sources.list && apt-get update && apt-get install sudo && usermod -a -G sudo egg"
