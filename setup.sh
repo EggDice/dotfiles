769c0d9d8ebbe131ffb183e5c406c017a737bdc7
Egg Dice <egg@BunnyBalloon>
2014-06-09 18:05:21 +0200

    trackpoint, docker

--- setup.sh
+++ setup.sh
@@ -6,6 +6,9 @@ sudo aptitude install -y vim git tmux
 sudo aptitude install lightdm i3
 sudo aptitude install -y ttf-mscorefonts-installer rxvt-unicode-256color
 sudo aptitude install -y nodejs
+sudo ln -sf /usr/bin/nodejs /usr/local/bin/node
+sudo aptitude install -y acpitool
+sudo aptitude install -y docker.io
 
 # Google Chrome
 wget -c wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
@@ -26,3 +29,9 @@ mkdir -p ~/downloads
 mkdir -p ~/music
 mkdir -p ~/projects
 mkdir -p ~/brickflow
+
+# TrackPoint
+sudo aptitude install xinput
+xinput set-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation" 1
+xinput set-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Button" 2
+xinput set-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Timeout" 200
