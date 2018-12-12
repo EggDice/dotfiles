4672e6ec4a05c4702fa321fb55916cb48f8fb519
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2018-12-12 16:38:13 +0100

    remove node form setup

--- setup.sh
+++ setup.sh
@@ -1,24 +1,13 @@
 # packages
-sudo apt-get install aptitude
-sudo aptitude update
-sudo aptitude upgrade
-sudo aptitude install -y vim-nox git tmux curl
-sudo aptitude install -y mercurial
-sudo aptitude install lightdm i3
-sudo aptitude install -y ttf-mscorefonts-installer rxvt-unicode-256color unzip
-sudo aptitude install -y nodejs npm ssh
-sudo ln -sf /usr/bin/nodejs /usr/local/bin/node
-sudo aptitude install -y acpitool
-sudo aptitude install -y docker.io
-sudo ln -sf /usr/bin/docker.io /usr/local/bin/docker
-sudo sed -i '$acomplete -F _docker docker' /etc/bash_completion.d/docker.io
-sudo aptitude install mongodb
-sudo aptitude install default-jre default-jdk
-sudo aptitude install transmission vlc mplayer moc
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install -y tmux curl
+sudo apt-get install -y ttf-mscorefonts-installer rxvt-unicode-256color
+sudo apt-get install -y docker.io
+sudo apt-get install default-jre default-jdk
+sudo apt-get install mplayer
 
-sudo aptitude install ruby-dev
-sudo aptitude install python
-sudo aptitude install python-pip
+# powerline
 pip install --user git+git://github.com/Lokaltog/powerline
 
 mkdir -p ~/.fonts
@@ -36,34 +25,9 @@ sudo dpkg -i google-chrome-*.deb
 sudo apt-get -y install -f
 rm google-chrome-*.deb
 
-# WiFi
-sudo aptitude install -y linux-headers-$(uname -r|sed 's,[^-]*-[^-]*-,,') broadcom-sta-dkms
-sudo modprobe -r b44 b43 b43legacy ssb brcmsmac
-sudo modprobe wl
-sudo usermod -G netdev -a egg
-
-# Desktop
-mkdir -p ~/video
-mkdir -p ~/pictures
-mkdir -p ~/downloads 
-mkdir -p ~/music
-mkdir -p ~/projects
-mkdir -p ~/brickflow
-
-# TrackPoint
-sudo aptitude install xinput
-xinput set-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation" 1
-xinput set-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Button" 2
-xinput set-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Timeout" 200
-
 # vim
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl 'www.vim.org/scripts/download_script.php?src_id=21650' > ~/.vim/autoload/pathogen.vim
 pushd ~/.vim/bundle/
 git clone https://github.com/scrooloose/syntastic.git
 popd
-sudo chown -R $USER /usr/local
-npm install -g jshint
-npm install -g grunt-cli
-gem install tmuxinator
-gem install lolcommits
