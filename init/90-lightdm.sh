c4ced59dfcb841ee0c997ec65ec02379816b5cef
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2018-12-13 23:34:21 +0100

    feat(lightdm): disable lock screen

--- init/90-lightdm.sh
+++ init/90-lightdm.sh
@@ -1,4 +1,6 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 # lightdm
-sudo ln -s $parent_path/../lightdm.conf /etc/lightdm/lightdm.conf
+sudo ln -s $parent_path/../lightdm.conf /etc/lightdm/lightdm.conf.d/lightdm.conf
+
+gsettings set org.gnome.desktop.screensaver lock-enabled false
