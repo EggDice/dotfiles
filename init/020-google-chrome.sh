bcb34d548699670ae44e0fb99d921a99d5a946ab
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2018-12-15 20:16:16 +0100

    feat(chrome): disable incognito

--- init/020-google-chrome.sh
+++ init/020-google-chrome.sh
@@ -3,4 +3,11 @@ sudo dpkg -i google-chrome-*.deb
 sudo apt-get -y install -f
 rm google-chrome-*.deb
 
+# Disable incognito
+mkdir -p /etc/opt/chrome/policies/managed/
+ln -s $parent_path/../test_policy.json /etc/opt/chrome/policies/managed/
+
+# hosts-file
+sudo rm /etc/hosts
+ln -s $parent_path/../hosts /etc/hosts
 
