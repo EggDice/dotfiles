0a770ee3caae628c0364cf5feab0f84e7e4e7868
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2018-12-25 01:29:29 +0100

    feat(urxvt): add Terminus font

---
+++ init/110-fonts.sh
@@ -0,0 +1,9 @@
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+# Install font
+git clone https://github.com/ryanoasis/nerd-fonts /var/tmp/nerd-fonts
+pushd /var/tmp/nerd-fonts
+./install.sh Terminus
+popd
+rm -r /var/tmp/nerd-fonts
+
