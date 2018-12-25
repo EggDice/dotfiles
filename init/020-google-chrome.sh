17de4687c83f1d744221c3d90cc37fe33d58f6e1
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2018-12-25 01:28:00 +0100

    fix(chrome): no incognito

--- init/020-google-chrome.sh
+++ init/020-google-chrome.sh
@@ -1,13 +1,15 @@
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
 wget -c wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-*.deb
 sudo apt-get -y install -f
 rm google-chrome-*.deb
 
 # Disable incognito
-mkdir -p /etc/opt/chrome/policies/managed/
-ln -s $parent_path/../test_policy.json /etc/opt/chrome/policies/managed/
+sudo mkdir -p /etc/opt/chrome/policies/managed/
+sudo ln -s $parent_path/../test_policy.json /etc/opt/chrome/policies/managed/
 
 # hosts-file
 sudo rm /etc/hosts
-ln -s $parent_path/../hosts /etc/hosts
+sudo ln -s $parent_path/../hosts /etc/hosts
 
