4876f599e157e4f3df42bc8af78e79c53242b347
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2019-01-25 14:23:42 +0100

    feat(xmonad): brightness buttons

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -25,6 +25,8 @@ main = xmonad $ defaultConfig {
       ((0, xF86XK_AudioLowerVolume), spawn "amixer set Master 2%-"),
       ((0, xF86XK_AudioRaiseVolume), spawn "amixer set Master 2%+"),
       ((0, xF86XK_AudioMute), spawn "amixer set Master toggle"),
+      ((0, xF86XK_MonBrightnessUp), spawn "xbacklight -inc 10"),
+      ((0, xF86XK_MonBrightnessDown), spawn "xbacklight -dec 10"),
       ((shiftMask, xK_Shift_R), spawn "(setxkbmap -query | grep -q 'layout:\\s\\+us') && setxkbmap hu || setxkbmap us")
     ]
 
