723d106da352fa17dbd8b2d8eea673d6eb459cb6
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2020-05-06 19:59:31 +0200

    fix(udiskie): move to pip3

--- init/080-udiskie.sh
+++ init/080-udiskie.sh
@@ -2,5 +2,5 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 # udiskie
 sudo apt-get -qq install python-setuptools python-pip python-gobject python-yaml libgio2.0 gobject-introspection libgtk2.0-0 libnotify4 gettext gir1.2-notify-0.7
-sudo pip install udiskie
+sudo pip3 install udiskie
 
