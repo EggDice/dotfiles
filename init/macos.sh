9e6ad09f51437f0050086bc96da29da98513b651
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2023-09-17 08:56:48 +0100

    Add brew programs

--- init/macos.sh
+++ init/macos.sh
@@ -18,11 +18,49 @@ fi
 
 # Brew
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/eggdice/.zprofile
-eval "$(/opt/homebrew/bin/brew shellenv)"
+if ! command -v brew &> /dev/null
+then
+    echo "Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/eggdice/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+    sourche ~/.zprofile
+else
+    echo "Homebrew already installed."
+fi
 
+# Brew Packages
+
+brew install --cask amethyst
 brew install --cask anki
+brew install cowsay
+brew install deno
+brew install --cask discord
+brew install --cask dosbox-x
+brew install ffmpeg
+brew install fzf
+brew install --cask google-chrome
+brew install gimp
+brew install --cask hyper
+brew install htop
+brew install imagemagick
+brew install --cask iina
+brew install --cask messenger
+brew install mmv
+brew install neovim
+brew install node
+brew install --cask obs
+brew install sl
+brew install --cask slack
+brew install sox
+brew install the_silver_searcher
+brew install thefuck
+brew install --cask transmission
+brew install tmux
+brew install tree
+brew install --cask wine-stable
+brew install --cask whatsapp
+
 
 # docker
 
@@ -31,7 +69,7 @@ brew install minikube
 brew install docker
 brew install docker-compose
 
-bash minikube start
+minikube start
 eval $(minikube docker-env)
 echo "`minikube ip` docker.local" | sudo tee -a /etc/hosts > /dev/null
 install docker-credential-helper
